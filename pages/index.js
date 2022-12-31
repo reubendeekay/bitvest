82dc34010bfc92eb3b6e02810b770a51be6b04e9
Reuben Jefwa <78743046+reubendeekay@users.noreply.github.com>
2022-12-31 13:01:12 +0300

    Trial on loading

--- pages/index.js
+++ pages/index.js
@@ -33,10 +33,10 @@ const CryptoModern = () => {
             name="keywords"
             content="React, React js, Next, Next js, Super fast next js landing, Modern landing, Next js landing"
           />
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
             rel="stylesheet"
-          />
+          /> */}
         </Head>
         {/* end of head */}
 
